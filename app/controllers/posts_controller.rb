ec30145717e178fbfc829602ba857c12cd7886a3
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-05-01 23:41:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,21 @@ class PostsController < ApplicationController
 	end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :description))
+    @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
  
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post).permit(:title))
+    @post.update(post_params)
     redirect_to post_path(@post)
+  end
+ 
+  private
+ 
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 
 	def edit
