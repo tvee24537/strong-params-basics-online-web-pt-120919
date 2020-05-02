98c21ff270fb44a3a847e173825a21dc047d2511
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-05-02 00:06:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
  
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
+    @post.update(post_params(:title))
     redirect_to post_path(@post)
   end
  
